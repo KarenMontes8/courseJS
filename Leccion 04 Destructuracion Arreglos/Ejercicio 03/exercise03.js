70817a2454a0e673bd79d98a43c8a600446553e4
KarenMontes8 <karenelgejdh@gmail.com>
2025-09-17 21:41:34 -0600

    Update exercise03.js

--- Leccion 04 Destructuracion Arreglos/Ejercicio 03/exercise03.js
+++ Leccion 04 Destructuracion Arreglos/Ejercicio 03/exercise03.js
@@ -2,7 +2,9 @@ const deck = [10, 7];
 
 function swapTwoCards(deck) {
   //Tu codigo aqui  👈;
+  [deck[0], deck[1]] = [deck[1], deck[0]];
+  return deck;
 }
 
-swapTwoCards(deck);
+console.log(swapTwoCards(deck));
 // => [7, 10]
